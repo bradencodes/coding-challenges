47185c47c74e98bb36cce83c29321e9c6050eb81
Braden Walker <bradencodes@gmail.com>
2019-02-08 18:03:41 -0800

    getting closer to fabricOverlap solution

--- fabricOverlap/mySolution1.js
+++ fabricOverlap/mySolution1.js
@@ -1,52 +1,39 @@
-let testFile = require('input_txt.txt');
-
 function countOverlap(file) {
     //create a 1000x1000 2d array filled with 0's
     let fabric = Array(1000).fill(Array(1000).fill(0));
     let overlap = 0;
   
     //parse the file line by line
-    let fs = require('fs'),
-        readline = require('readline'),
-        instream = fs.createReadStream(file),
-        outstream = new (require('stream'))(),
-        rl = readline.createInterface(instream, outstream);
+    // let fs = require("fs");
+    // let textByLine = fs.readFileSync(file).toString().split('\n');
+    let textByLine = file.split('\n');
 
     //for every line, parse the values from it as follows...
     // #1 @ 1,3: 2x4 -> c=1 r=3 w=2 h=4
-    rl.on('line', function (line) {
-        let re = /#\d+\s@\s(\d+),(\d+):\s(\d+)x(\d+)/g;
-        let parsed = re.exec(line);
-        let c = parsed[0];
-        let r = parsed[1];
-        let w = parsed[2];
-        let h = parsed[3];
+    for(let i=0 ; i<textByLine.length ; i++) {
+        let line = textByLine[i]; //?
+        let re = /(\d+),(\d+):\s(\d+)x(\d+)/g;
+        let parsed = re.exec(line); //?
+        let c = parseInt(parsed[1]); //?
+        let r = parseInt(parsed[2]); //?
+        let w = parseInt(parsed[3]); //?
+        let h = parseInt(parsed[4]); //?
 
-        for (; c<c+w ; c++) {
-            for(; r<r+h ; r++) {
-                fabric[r][c]++;
-                if (fabric[r][c] === 2) overlap++;
+        for (let x=c ; x<c+w ; x++) {
+            for(let y=r ; y<r+h ; y++) {
+                console.log(y);
+                console.log(x);
+                fabric[y][x]++; //?
+                if (fabric[y][x] === 2) overlap++;
             }
         }
-    });
-
-    rl.on('close', function (line) {
-        let re = /#\d+\s@\s(\d+),(\d+):\s(\d+)x(\d+)/g;
-        let parsed = re.exec(line);
-        let c = parsed[0];
-        let r = parsed[1];
-        let w = parsed[2];
-        let h = parsed[3];
-
-        for (; c<c+w ; c++) {
-            for(; r<r+h ; r++) {
-                fabric[r][c]++;
-                if (fabric[r][c] === 2) overlap++;
-            }
-        }
-    });
+    }
         
     return overlap;
 }
 
-console.log(countOverlap(testFile));
+let testFile = 'input.txt';
+
+let testText = "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2"
+
+console.log(countOverlap(testText));
