0c14114a79fb5b66913b2d7d88e9fee27ce53469
Braden Walker <bradencodes@gmail.com>
2019-02-12 15:56:55 -0800

    add mySolution2 to validateBinaryTree

---
+++ validateBinaryTree/mySolution2.js
@@ -0,0 +1,34 @@
+class BinaryTreeNode {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  
+    insertLeft(value) {
+      this.left = new BinaryTreeNode(value);
+      return this.left;
+    }
+  
+    insertRight(value) {
+      this.right = new BinaryTreeNode(value);
+      return this.right;
+    }
+  }
+
+//===== solution 2 =====
+function isBinarySearchTree2(root) {
+    let array = [];
+  
+    //convert tree to array by pushing left, then value, then right recursively
+    function treeToArray(tree) {
+      if (tree.left) treeToArray(tree.left);
+      if (array.length > 0 && array[array.length-1] > tree.value) return false;
+      array.push(tree.value);
+      if (tree.right) treeToArray(tree.right);
+  
+      return true;
+    }
+  
+    return treeToArray(root);
+  }
