0e569fcfc4ad4330f1f70108966f69ae6e922a1e
Braden Walker <bradencodes@gmail.com>
2019-03-01 08:25:35 -0800

    add ProductOfAllOtherNumber

---
+++ ProductOfAllOtherNumbers/mySolution1.js
@@ -0,0 +1,27 @@
+function paon(arr) {
+    //make a big array of length arr.length with an array at each index
+    let bigArray = [];
+    for (let i = 0 ; i < arr.length ; i++){
+      bigArray.push([].concat(arr));
+    }
+  
+    //loop through the big array and remove arr[i]
+    for (let i = 0 ; i < bigArray.length ; i++){
+      bigArray[i].splice(i, 1);
+  
+      //after that, reduce the sub array to the product of all numbers
+      let multiply = (a, b) => a*b;
+      bigArray[i] = bigArray[i].reduce(multiply);
+    }
+  
+    //return the big array
+    return bigArray;
+  }
+  
+  console.log(paon(
+    [1, 2, 3, 4, 5]
+  ));   // should print [120, 60, 40, 30, 24]
+  
+  console.log(paon(
+    [9, 90]
+  ));   // should print [90, 9]
