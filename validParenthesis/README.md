f96cb2ec48aa16386d20afa7aad5fda0b8dbddee
Braden Walker <bradencodes@gmail.com>
2019-03-27 10:58:19 -0700

    add validParenthesis

---
+++ validParenthesis/README.md
@@ -0,0 +1,3 @@
+URL: https://www.codewars.com/kata/52774a314c2333f0a7000688/train/javascript
+
+Write a function called that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
---
+++ validParenthesis/mySolution1.js
@@ -0,0 +1,21 @@
+function validParentheses(parens){
+    let open = []; //stack of open parenthesis
+    let matches = {
+        ")": "(",
+        "]": "[",
+        "}": "{",
+    }
+    let starr = parens.split(''); //an array of each character
+
+    for (let i = 0 ; i < starr.length ; i++){
+        let char = starr[i];
+        if (char === '(' || char === '{' || char === '[') open.push(char);
+
+        // Return false if the closed character doesn't match the last open character
+        else if (matches[char] && matches[char] !== open.pop()) return false;
+    }
+
+    // Return false if there are open characters that haven't been closed.
+    // Otherwise it is valid.
+    return open.length === 0;
+    }
