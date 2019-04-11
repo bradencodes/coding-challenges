455ec6802820002e6cf0aab11c736bdd8855ccec
Braden Walker <bradencodes@gmail.com>
2019-04-11 11:10:14 -0700

    add postorder traversal iterative solution

--- LEETCODE_LEARN/BinaryTree/postorder_traversal.js
+++ LEETCODE_LEARN/BinaryTree/postorder_traversal.js
@@ -1,3 +1,30 @@
+//iterative
+var postorderTraversal = function(root) {
+    if (!root) return [];
+    let result = [];
+    let stack = [];
+    let current = root;
+    do {
+        while(current){
+            if (current.right) stack.push(current.right);
+            stack.push(current);
+            current = current.left ? current.left : null;        
+        }
+        current = stack.pop();
+        if (current.right && current.right === stack[stack.length-1]){
+            stack.pop();
+            stack.push(current);
+            current = current.right;
+        }
+        else {
+            result.push(current.val);
+            current = null;
+        }
+    } while(stack.length);
+    
+    return result;
+};
+
 //recursive
 var postorderTraversal = function(root) {
     if (!root) return [];
