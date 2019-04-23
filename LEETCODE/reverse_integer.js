d3f7e368ab8f4959356560ab80050cbc3ff53753
Braden Walker <bradencodes@gmail.com>
2019-04-23 08:59:47 -0700

    add revision to reverse_integer

--- LEETCODE/reverse_integer.js
+++ LEETCODE/reverse_integer.js
@@ -7,3 +7,17 @@ var reverse = function(x) {
     ans = num.toString().split('').reverse().join('');
     return ans > 2147483648 ? 0 : ans * sign;
 };
+
+// Revised Solution:
+var reverse = function(x) {
+    let sign = x < 0 ? -1 : 1;
+    let num = x * sign;
+    let ans = 0;
+    while (num){
+        let digit = num % 10;
+        num = (num - digit) / 10;
+        ans = ans * 10 + digit;
+    }
+    maxInt = 2147483648;
+    return ans > maxInt ? 0 : ans * sign;
+};
